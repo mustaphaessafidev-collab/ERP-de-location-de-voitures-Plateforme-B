740f25cd48d847eb03e1231ae595248f9c060927
Mustapha Essafi <mustapha.essafi.dev@gmail.com>
2026-04-08 14:37:22 +0100

    notification sever comleted

--- Frontend/src/router/createAppRouter.jsx
+++ Frontend/src/router/createAppRouter.jsx
@@ -29,7 +29,7 @@ const publicRoutes = mapRoutesToRouter([
   },
   { path: "/VehicleCatalogPage", element: <VehicleCatalogPage /> },
   { path: "/VehicleDetail/:id", element: <VehicleDetailsPage /> },
-  
+  { path: "/notifications", element: <NotificationsPage /> },
 
 ]);
 
@@ -81,7 +81,7 @@ const privateRoutes = mapRoutesToRouter([
   },
 
   
-  { path: "/notifications", element: <NotificationsPage /> },
+  
   { path: "/test-success", element: <PlaceholderPage title="Test success" message="Test success message" /> },
   { path: "/reservation", element: <ReservationPage /> },
   { path: "/booking-review", element: <ReservationPage /> },
