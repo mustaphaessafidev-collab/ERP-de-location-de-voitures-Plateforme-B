3b9da20d39c06a803cb081b38b1648c23df40e65
Khalid Demnati <kdemnati20@gmail.com>
2026-04-18 12:45:06 +0100

    checkment total

--- services/auth-service/src/routes/user.routes.ts
+++ services/auth-service/src/routes/user.routes.ts
@@ -4,4 +4,4 @@ import { userController } from "../controllers/user.controller";
 export const userRouter = Router();
 
 userRouter.get("/", userController.list);
-userRouter.get("/:id/", userController.getById);
+userRouter.get("/:id", userController.getById);
